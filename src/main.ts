873425cd996bc85788e4cca7bfa4b6cf121cb437
tanmayhire26 <96424507+tanmayhire26@users.noreply.github.com>
2024-10-04 18:36:11 +0530

    [bot]

--- src/main.ts
+++ src/main.ts
@@ -2,24 +2,28 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
+const allowedOrigins = ['http://localhost:3000'];
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { abortOnError: false });
-  const config = new DocumentBuilder()
-    .setTitle('Example')
-    .setDescription('Cash-flo APIs examples')
-    .setVersion('1.0.0')
-    .addTag('')
-    .build();
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: allowedOrigins,
     methods: 'GET,PUT,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept',
     credentials: true,
   });
 
+  const config = new DocumentBuilder()
+    .setTitle('Example')
+    .setDescription('Cash-flo APIs examples')
+    .setVersion('1.0.0')
+    .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
   await app.listen(7007);
 }
+
 bootstrap();
--- src/transactions/transactions.controller.ts
+++ src/transactions/transactions.controller.ts
@@ -1,17 +1,9 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-  Req,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Req, UseGuards, UnauthorizedException } from '@nestjs/common';
 import { TransactionsService } from './transactions.service';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { UpdateTransactionDto } from './dto/update-transaction.dto';
 import { Request } from 'express';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('transactions')
 export class TransactionsController {
@@ -46,18 +38,15 @@ export class TransactionsController {
   }
 
   @Post('get-PR-data')
-  getPrData(
-    @Req() req: Request
-  ) {
-    try {
-      const eventType = req.headers['x-github-event'];
-      if (eventType === 'pull_request') {
-          const pullRequestData = req.body;
-          console.log('Pull Request Event Received:', pullRequestData);
-          // Handle pull request data as needed
-      }
-    } catch (error) {
-      throw error
+  @UseGuards(AuthGuard('jwt'))
+  async getPrData(@Req() req: Request) {
+    const eventType = req.headers['x-github-event'];
+    if (eventType === 'pull_request') {
+      const pullRequestData = req.body;
+      console.log('Pull Request Event Received:', pullRequestData);
+      // Handle pull request data as needed
+    } else {
+      throw new UnauthorizedException('Invalid event type');
     }
   }
 }
