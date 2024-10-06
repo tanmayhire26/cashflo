1f8b0342cacc73d88606f4d08ed5a361d0fcacce
tanmayhire <tanmay.hire@gmail.com>
2024-10-06 20:38:50 +0530

    refactored code by bot

--- src/transactions/transactions.controller.ts
+++ src/transactions/transactions.controller.ts
@@ -1,63 +1,51 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-  Req,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Req, UseGuards } from '@nestjs/common';
 import { TransactionsService } from './transactions.service';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { UpdateTransactionDto } from './dto/update-transaction.dto';
 import { Request } from 'express';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('transactions')
 export class TransactionsController {
   constructor(private readonly transactionsService: TransactionsService) {}
 
+  @UseGuards(AuthGuard('jwt'))
   @Post()
   async create(@Body() createTransactionDto: CreateTransactionDto) {
     return await this.transactionsService.create(createTransactionDto);
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Get()
   async findAll() {
     return await this.transactionsService.findAll();
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.transactionsService.findOne(+id);
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateTransactionDto: UpdateTransactionDto,
-  ) {
+  update(@Param('id') id: string, @Body() updateTransactionDto: UpdateTransactionDto) {
     return this.transactionsService.update(+id, updateTransactionDto);
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.transactionsService.remove(+id);
   }
 
   @Post('get-PR-data')
-  getPrData(
-    @Req() req: Request
-  ) {
-    try {
-      const eventType = req.headers['x-github-event'];
-      if (eventType === 'pull_request') {
-          const pullRequestData = req.body;
-          console.log('Pull Request Event Received:', pullRequestData);
-          // Handle pull request data as needed
-      }
-    } catch (error) {
-      throw error
+  async getPrData(@Req() req: Request) {
+    const eventType = req.headers['x-github-event'] as string;
+    if (eventType === 'pull_request') {
+      const pullRequestData = req.body;
+      console.log('Pull Request Event Received:', pullRequestData);
+      // Handle pull request data as needed
     }
   }
 }
