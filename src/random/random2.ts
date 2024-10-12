cdc22b23a3ba633c50e9ec6ed4261706ff7a4d35
tanmayhire <tanmay.hire@gmail.com>
2024-10-12 15:43:32 +0530

    refactored code by bot

--- src/random/random2.ts
+++ src/random/random2.ts
@@ -1,9 +1,9 @@
 const gnrteRndmWrd = () => {
-    let ltrs = "abcdefghijklmnopqrstuvwxyz"; 
+    let letters = "abcdefghijklmnopqrstuvwxyz"; 
     let wrdLth = Math.floor(Math.random() * 10) + 3; 
     let wrd = ""; 
     
-    wrd = Array(wrdLth).fill(0).map(() => ltrs.charAt(Math.floor(Math.random() * ltrs.length))).join('');
+    wrd = Array(wrdLth).fill(0).map(() => letters.charAt(Math.floor(Math.random() * letters.length))).join('');
 
     return wrd; 
 }
