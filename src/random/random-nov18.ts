09d3d7c63783547b2a270ce535ee910a181f73f3
tanmayhire <tanmay.hire@gmail.com>
2024-11-19 18:51:37 +0530

    refactored code by bot

--- src/random/random-nov18.ts
+++ src/random/random-nov18.ts
@@ -1,8 +1,6 @@
 function removeDuplicates<T>(array: T[], key: keyof T): T[] {
     const uniqueObjects = new Map<any, T>();
-    for (let i = 0; i < array.length; i++) {
-        uniqueObjects.set(array[i][key], array[i]);
-    }
+    array.forEach(item => uniqueObjects.set(item[key], item));
     return Array.from(uniqueObjects.values());
 }
 
